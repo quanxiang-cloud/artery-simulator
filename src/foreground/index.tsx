8050484e3ca6f2dab3a40e391ef2325e1d7b725d
Mark <markduan@yunify.com>
2022-04-29 22:24:34 +0800

    refactor: Merge files

--- src/foreground/index.tsx
+++ src/foreground/index.tsx
@@ -1,17 +1,42 @@
-import React from 'react';
+import { useRecoilState, useSetRecoilState } from 'recoil';
+import React, { useEffect } from 'react';
 
-import ContourNodes from './contour-nodes';
+import RenderContourNode from './render-contour-node';
+import Toolbar from './toolbar';
+import { contourNodesState, isScrollingState } from '../atoms';
+
 import type { SimulatorReport } from '../types';
 
 import './index.scss';
+import { useContourNodes } from './use-contour-nodes';
 
 interface Props {
   report: SimulatorReport;
 }
 
 function Foreground({ report }: Props): JSX.Element {
+  const [isScrolling] = useRecoilState(isScrollingState);
+  const contourNodes = useContourNodes(report.visibleNodes, isScrolling);
+
+  const setContourNodesState = useSetRecoilState(contourNodesState);
+  useEffect(() => {
+    setContourNodesState(contourNodes);
+  }, [contourNodes]);
+
   return (
-    <ContourNodes nodes={report.visibleNodes} />
+    <>
+      <div className="contour-nodes">
+        {contourNodes.map((contour) => {
+          return (
+            <RenderContourNode
+              key={`contour-${contour.id}`}
+              contourNode={contour}
+            />
+          );
+        })}
+      </div>
+      <Toolbar />
+    </>
   );
 }
 
